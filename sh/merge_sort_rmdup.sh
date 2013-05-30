391f5674f162667e3d9e6fd40c77c97d97599cc0
Graham McVicker <gmcvicker@uchicago.edu>
2013-05-30 17:21:36 -0500

    A lot of work on updating README, a few small tweaks to scripts to make more usable by other people.

--- sh/merge_sort_rmdup.sh
+++ sh/merge_sort_rmdup.sh
@@ -7,7 +7,7 @@ for type in PolII H3K4me3 H3K4me1 H3K27ac H3K27me3
     for chr in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
       do
       echo "zcat ${head}/mapper/mapped_reads/${type}/${type}_H_${ind}*/chr${chr}.mapped.txt.gz | sort -k 3n > ${head}/filtered/${type}/${ind}.chr${chr}.temp; \
-python /home/bmvdgeijn/Brycetools/gmc_file_rmdup.py ${head}/filtered/${type}/${ind}.chr${chr}.temp ${head}/filtered/${type}/${ind}.chr${chr}.txt; \
+python ../../rmdup.py ${head}/filtered/${type}/${ind}.chr${chr}.temp ${head}/filtered/${type}/${ind}.chr${chr}.txt; \
 gzip -f ${head}/filtered/${type}/${ind}.chr${chr}.txt; \
 rm ${head}/filtered/${type}/${ind}.chr${chr}.temp " | qsub -l h_vmem=4g -cwd -V
     done
